6b0504c8b5bcaf81e4e341d13f23a02684e1395f
Alasdair Reid <ali.reid@live.co.uk>
2022-01-03 12:17:53 +0000

    Initial commit
    Basic .net core 3 MVC calculator

---
+++ NetCoreCalculator/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreCalculator.Models;
+
+namespace NetCoreCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+            model.Result = model.OperationType switch
+            {
+                OperationType.Addition => model.NumberA + model.NumberB,
+                OperationType.Multiplication => model.NumberA * model.NumberB,
+                OperationType.Division => model.NumberA / model.NumberB,
+                OperationType.Subtraction => model.NumberA - model.NumberB,
+                _ => 0,
+            };
+            return View(model);
+        }
+    }
+}
